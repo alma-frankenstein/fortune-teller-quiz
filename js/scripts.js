5101a3b98756d868cdcf1ce5b6d11aba48153a78
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-18 11:50:23 -0700

    Add js to display future

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-$(document.).ready(function() {
-  $("#events").submit
+$(document).ready(function() {
+  $("#events").submit(function(event) {
+    event.preventDefault();
+    let unluckyEventsArray = []
+    let luckyEventsArray = []
+    $("input:checkbox[name=unlucky-events]:checked").each(function() {
+      const unluckyEvents = $(this).val();
+      unluckyEventsArray.push(unluckyEvents);
+    })
+    $("input:checkbox[name=lucky-events]:checked").each(function() {
+      const luckyEvents = $(this).val();
+      luckyEventsArray.push(luckyEvents);
+    })
+
+    function fortune(unluckyEventsArray, luckyEventsArray) {
+      let fortune;
+      if (unluckyEventsArray.length > luckyEventsArray.length) {
+        fortune = "You are going to have a terrible week." 
+      }
+      else if (luckyEventsArray.length > unluckyEventsArray.length) {
+        fortune = "You are going to have a fantastic week!!!!"
+      }
+      else {
+        fortune = "Your week will be meh."
+      }
+      return fortune;
+    }
+
+    let newFortune = fortune(unluckyEventsArray, luckyEventsArray);
+    $("#results").text(newFortune);
+    $("#results").show();
+  })
 })
